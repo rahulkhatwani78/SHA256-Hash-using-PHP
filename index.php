f47a6bbaac46d26404038a1775ac4b51d9aedc88
rahul khatwani <rahulkhatwani78@gmail.com>
2020-08-01 13:29:03 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rahul Khatwani PHP</title>
+</head>
+<body>
+    <h1>Rahul Khatwani PHP</h1>
+    <p>
+        The SHA256 hash of "Rahul Khatwani" is
+        <?php
+            print hash('sha256', 'Rahul Khatwani');
+        ?>
+    </p>
+</body>
+</html>
